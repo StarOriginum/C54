24199e6be3a307973f71f307c8e554bdd3607828
MichaelTurner.Affaires@hotmail.com <MichaelTurner.Affaires@hotmail.com>
2024-10-16 11:15:24 -0400

    correction examen 1

--- examen1_kt/app/src/main/java/com/example/examen1_kt/Langages.kt
+++ examen1_kt/app/src/main/java/com/example/examen1_kt/Langages.kt
@@ -1,5 +1,7 @@
 package com.example.examen1_kt
 
-data class Langages(var nom:String, var classement2024:Int, var classement2019:Int, var classement2012:Int) {
+import java.io.Serializable
+
+data class Langages(var nom:String, var classement2024:Int, var classement2019:Int, var classement2012:Int):Serializable {
 
 }
--- examen1_kt/app/src/main/java/com/example/examen1_kt/MainActivity.kt
+++ examen1_kt/app/src/main/java/com/example/examen1_kt/MainActivity.kt
@@ -8,17 +8,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.io.BufferedReader
 import java.io.FileInputStream
+import java.io.FileNotFoundException
 import java.io.InputStreamReader
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Vector
 
 class MainActivity : AppCompatActivity() {
-    lateinit var langages: Langages
+    lateinit var langages: Vector<Langages>
     lateinit var nbChamp: TextView
     lateinit var classement2012:TextView
+    lateinit var texteDate:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,42 +32,78 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        lectureFichierLangage()
+        langages = Vector()
+        texteDate = findViewById(R.id.texteDate)
+
+        try {
+            val derniereFois = deserialisation()
+            val formatter = DateTimeFormatter.ofPattern("hh:mm dd MM yyyy")
+            texteDate.setText("derniere utilisation: " + derniereFois.format(formatter))
+        } catch (e: FileNotFoundException){
+            texteDate.setText("1ere utilisation")
+        }
 
         nbChamp = findViewById(R.id.champClass2012)
         classement2012 = findViewById(R.id.Class2012)
 
 
+        lectureFichierLangage()
 
+        nbChamp.setText(trouver99())
 
 
 
 
     }
-    fun lectureFichierLangage(){
-        var classements:Vector<Langages> = Vector()
-        var compteur:Int = 0
+
+
+    override fun onStop() {
+        super.onStop()
+        serialisation()
+    }
+    fun lectureFichierLangage() {
+
+
 
         val fis: FileInputStream = openFileInput("langage.txt")
-        val isw = InputStreamReader(fis)
-        val br = BufferedReader(isw)
+        val obj = InputStreamReader(fis)
+        val br = BufferedReader(obj)
+        br.readLine()
+        fis.bufferedReader().use {
+            var line = br.readLine()
 
-        while (br.readLine() != null){
-            classements.add(langages)
-
-            if(langages.classement2012 == 99)
-                compteur++
+            while (line != null) {
+                var tab = line.split(",")
+                langages.add(Langages(tab[0], tab[1].toInt(), tab[2].toInt(), tab[3].toInt()))
+                line = br.readLine()
+            }
         }
-        classement2012.text = compteur.toString()
     }
 
-    fun afficherHeures(){
-        val date =
+    fun trouver99():Int{
+        var compteur:Int = 0
+        for(l:Langages in langages){
+            if (l.classement2012 == 99)
+                compteur++
+        }
+        return compteur
+    }
+
+    fun serialisation(){
 
         val fos = openFileOutput("date.ser", MODE_PRIVATE)
         val oos = ObjectOutputStream(fos)
-        oos.use {
-            it.writeObject(date)
+
+        oos.use {oos ->
+            oos.writeObject(LocalDateTime.now())
+        }
+    }
+
+    fun deserialisation(): LocalDateTime {
+        openFileInput("date.ser").use { fos ->
+            ObjectInputStream(fos).use { oos ->
+                return oos.readObject() as LocalDateTime
+            }
         }
     }
 }
