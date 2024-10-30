e169416dad26a67adec137b80a6467d68f8bd008
MichaelTurner.Affaires@hotmail.com <MichaelTurner.Affaires@hotmail.com>
2024-10-30 08:13:10 -0400

    Ajout des plusieurs fonctionnalites du tp

--- TP1-Musique/app/src/main/java/com/example/tp1_musique/AccessServeur.java
+++ TP1-Musique/app/src/main/java/com/example/tp1_musique/AccessServeur.java
@@ -9,56 +9,65 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import android.os.Handler;
-
-
+import android.os.Looper;
 
 
 public class AccessServeur implements Sujet {
 
-    String url = "https://api.jsonbin.io/v3/b/661ab8b1acd3cb34a837f284?meta=false";
-    Gson gson = new Gson();
+    private final String url = "https://api.jsonbin.io/v3/b/661ab8b1acd3cb34a837f284?meta=false";
+    private final Gson gson = new Gson();
 
-    private RequestQueue requestQueue;
+    private final RequestQueue requestQueue;
+    private ListeChansons lp;
+    private ObservateurChangement obs;
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
     public AccessServeur(Context context){
         this.requestQueue = Volley.newRequestQueue(context);
     }
 
     public void fetchChansons(String url) {
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
-            ListeChansons lc = gson.fromJson(response, ListeChansons.class);
 
-            final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+                lp = gson.fromJson(response, ListeChansons.class);
 
-            handler.postDelayed(new Thread() {
-                @Override
-                public void run(){
-                    avertirObservateurs();
-                }
-            }, 1000);
+                final Handler handler = new Handler();
 
-            /*setChanged();
-            notifyObservers(lc);*/
-        }, error -> {
-            error.printStackTrace();
-        });
+                handler.postDelayed(new Thread() {
+                    @Override
+                    public void run(){
+                        avertirObservateurs();
+                    }
+                }, 1000);
 
-        requestQueue.add(stringRequest);
+            }, error -> {
+                error.printStackTrace();
+            });
+
+            requestQueue.add(stringRequest);
+        }, 1000);
+
     }
 
 
     @Override
     public void ajouterObservateur(ObservateurChangement obs) {
-
+        this.obs = obs;
     }
 
     @Override
     public void enleverObservateur(ObservateurChangement obs) {
+        obs = null;
 
     }
 
     @Override
     public void avertirObservateurs() {
+        if (obs != null){
+            obs.changement(lp);
+        }
+
 
     }
 }
--- TP1-Musique/app/src/main/java/com/example/tp1_musique/MainActivity.java
+++ TP1-Musique/app/src/main/java/com/example/tp1_musique/MainActivity.java
@@ -1,30 +1,44 @@
 package com.example.tp1_musique;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.media3.common.MediaItem;
+import androidx.media3.common.Player;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.ui.PlayerView;
 
-import java.util.Observable;
-import java.util.Observer;
-import java.util.concurrent.atomic.AtomicReference;
+public class MainActivity extends AppCompatActivity implements ObservateurChangement {
 
-public class MainActivity extends AppCompatActivity {
-    AccessServeur accessServeur;
     ExoPlayer exoPlayer;
+    SeekBar seekBar;
     PlayerView vue;
+    ImageView playPauseButton, nextButton, prevButton;
+    TextView titreChanson;
+    Handler handler = new Handler();
+    Runnable miseAJourSeekBar;
+
+    ListeChansons listeChansons;
+    int indexChansons = 0;
+    long savedPosition = 0;
+    AccessServeur accessServeur;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.parent), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -33,8 +47,163 @@ public class MainActivity extends AppCompatActivity {
         });
 
         vue.setUseController(false);
-        accessServeur =  new AccessServeur(this);
+
+
+        /*seekBar = findViewById(R.id.seekBar);
+        playPauseButton = findViewById(R.id.playPauseButton);
+        nextButton = findViewById(R.id.nextButton);
+        prevButton = findViewById(R.id.prevButton);
+        titreChanson = findViewById(R.id.titleTextView);*/
+
+        exoPlayer = new ExoPlayer.Builder(this).build();
+        exoPlayer.addListener(new Player.Listener() {
+            @Override
+            public void onPlaybackStateChanged(int playbackState) {
+                if (playbackState == Player.STATE_ENDED) {
+                    skipToNextSong();
+                }
+            }
+
+        });
+
+        accessServeur = new AccessServeur(this);
+        accessServeur.ajouterObservateur(this);
+
+        setUpSeekBar();
+        setUpSong(indexChansons);
+
+        playPauseButton.setOnClickListener(v -> togglePlayPause());
+        nextButton.setOnClickListener(v -> skipToNextSong());
+        prevButton.setOnClickListener(v -> skipToPreviousSong());
     }
 
+    @Override
+    public void changement(ListeChansons music) {
+        this.listeChansons = music;
+        setUpSong(indexChansons);
+    }
 
+    public void setUpSong(int index){
+        if (index >= 0 && index < listeChansons.getMusic().size()){
+            Chansons song = listeChansons.getMusic().get(index);
+
+            MediaItem mediaItem = MediaItem.fromUri(song.getSource());
+            exoPlayer.setMediaItem(mediaItem);
+            exoPlayer.prepare();
+            exoPlayer.play();
+
+            titreChanson.setText(song.getTitle());
+            seekBar.setMax((int) song.getDuration());
+
+            seekBar.setProgress(0);
+
+            startSeekBarUpdate();
+        }
+    }
+
+    public void startSeekBarUpdate(){
+        miseAJourSeekBar = new Runnable() {
+            @Override
+            public void run() {
+                if (exoPlayer != null && exoPlayer.isPlaying()){
+                    seekBar.setProgress((int) exoPlayer.getCurrentPosition());
+                }
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(miseAJourSeekBar);
+    }
+
+    public void stopSeekBarUpdate(){
+        handler.removeCallbacks(miseAJourSeekBar);
+    }
+
+    public void togglePlayPause(){
+        if (exoPlayer.isPlaying()){
+            exoPlayer.pause();
+            stopSeekBarUpdate();
+        }
+        else{
+            exoPlayer.play();
+            startSeekBarUpdate();
+        }
+    }
+
+    public void skipToNextSong(){
+        stopSeekBarUpdate();
+        indexChansons = (indexChansons +1) % listeChansons.getMusic().size();
+        setUpSong(indexChansons);
+    }
+
+    public void skipToPreviousSong(){
+        stopSeekBarUpdate();
+        indexChansons = (indexChansons -1 + listeChansons.getMusic().size()) % listeChansons.getMusic().size();
+        setUpSong(indexChansons);
+    }
+
+    public void setUpSeekBar(){
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekbar, int progress, boolean fromUser){
+                if(fromUser && exoPlayer != null){
+                    exoPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                stopSeekBarUpdate();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                startSeekBarUpdate();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopSeekBarUpdate();
+        exoPlayer.release();
+        accessServeur.enleverObservateur(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        if (exoPlayer != null){
+            long currentPosition = exoPlayer.getCurrentPosition();
+            outState.putLong("currentPosition", currentPosition);
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null && exoPlayer != null){
+            long currentPosition = savedInstanceState.getLong("currentPosition", 0);
+            exoPlayer.seekTo(currentPosition);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (exoPlayer != null && exoPlayer.isPlaying()){
+            exoPlayer.pause();
+            savedPosition = exoPlayer.getCurrentPosition();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (exoPlayer != null && savedPosition > 0){
+            exoPlayer.seekTo(savedPosition);
+            exoPlayer.play();
+        }
+    }
 }
